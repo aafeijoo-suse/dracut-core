03597e3b68b3247c8f166b3d4b01e73f53dabd08
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2024-07-12 13:58:51 +0200

    chore: remove references to connman

--- modules.d/40network/module-setup.sh
+++ modules.d/40network/module-setup.sh
@@ -9,7 +9,7 @@ check() {
 depends() {
     is_qemu_virtualized && echo -n "qemu-net "
 
-    for module in connman network-manager network-legacy systemd-networkd; do
+    for module in network-manager network-legacy systemd-networkd; do
         if dracut_module_included "$module"; then
             network_handler="$module"
             break
@@ -17,9 +17,7 @@ depends() {
     done
 
     if [ -z "$network_handler" ]; then
-        if check_module "connman"; then
-            network_handler="connman"
-        elif check_module "network-manager"; then
+        if check_module "network-manager"; then
             network_handler="network-manager"
         elif check_module "systemd-networkd"; then
             network_handler="systemd-networkd"
