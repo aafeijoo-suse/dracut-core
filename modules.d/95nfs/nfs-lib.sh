04e86945e496102e4456045ebd595d8fb7ed69aa
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2024-11-13 11:23:30 +0100

    refactor(nfs): simplify nfs-lib

--- modules.d/95nfs/nfs-lib.sh
+++ modules.d/95nfs/nfs-lib.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 
 type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
-type get_ip > /dev/null 2>&1 || . /lib/net-lib.sh
 
-# TODO: make these things not pollute the calling namespace
-
-# nfs_to_var NFSROOT [NETIF]
+# nfs_to_var NFSROOT
 # use NFSROOT to set $nfs, $server, $path, and $options.
 # NFSROOT is something like: nfs[4]:<server>:/<path>[:<options>|,<options>]
-# NETIF is used to get information from DHCP options, if needed.
 nfs_to_var() {
     # Unfortunately, there's multiple styles of nfs "URL" in use, so we need
     # extra functions to parse them into $nfs, $server, $path, and $options.
-    # FIXME: local netif=${2:-$netif}?
     case "$1" in
         nfs://*) rfc2224_nfs_to_var "$1" ;;
-        nfs:*[*) anaconda_nfsv6_to_var "$1" ;;
-        nfs:*:*:/*) anaconda_nfs_to_var "$1" ;;
         *) nfsroot_to_var "$1" ;;
     esac
-    # if anything's missing, try to fill it in from DHCP options
-    if [ -z "$server" ] || [ -z "$path" ]; then nfsroot_from_dhcp "$2"; fi
-    # if there's a "%s" in the path, replace it with the hostname/IP
-    if strstr "$path" "%s"; then
-        local node=""
-        read -r node < /proc/sys/kernel/hostname
-        [ "$node" = "(none)" ] && node=$(get_ip "$2")
-        path=${path%%%s*}$node${path#*%s} # replace only the first %s
-    fi
 }
 
 # root=nfs:[<server-ip>:]<root-dir>[:<nfs-options>]
@@ -72,51 +56,6 @@ rfc2224_nfs_to_var() {
     server="${server%%:}" # anaconda compat (nfs://<server>:/<path>)
     local port="${server##*:}"
     [ "$port" != "$server" ] && options="port=$port"
-}
-
-# Anaconda-style path with options: nfs:<options>:<server>:/<path>
-# (without mount options, anaconda is the same as dracut)
-anaconda_nfs_to_var() {
-    nfs="nfs"
-    options="${1#nfs:}"
-    server="${options#*:}"
-    server="${server%:/*}"
-    options="${options%%:*}"
-    path="/${1##*:/}"
-}
-
-# IPv6 nfs path will be treated separately
-anaconda_nfsv6_to_var() {
-    nfs="nfs"
-    path="$1"
-    options="${path#*:/}"
-    path="/${options%%:*}"
-    server="${1#*nfs:}"
-    if str_starts "$server" '['; then
-        server="${server%:/*}"
-        options="${options#*:*}"
-    else
-        server="${server%:/*}"
-        options="${server%%:*}"
-        server="${server#*:}"
-    fi
-}
-
-# nfsroot_from_dhcp NETIF
-# fill in missing server/path from DHCP options.
-nfsroot_from_dhcp() {
-    local f
-    for f in /tmp/net."$1".override /tmp/dhclient."$1".dhcpopts /tmp/leaseinfo."$1".dhcp.*; do
-        # shellcheck disable=SC1090
-        [ -f "$f" ] && . "$f"
-    done
-    [ -n "$new_root_path" ] && nfsroot_to_var "$nfs:$new_root_path"
-    [ -z "$path" ] && [ "$(getarg root=)" = "/dev/nfs" ] && path=/tftpboot/%s
-    [ -z "$server" ] && server=$srv
-    [ -z "$server" ] && server=$new_next_server
-    [ -z "$server" ] && server=$new_dhcp_server_identifier
-    [ -z "$server" ] && server=$BOOTSERVERADDR
-    [ -z "$server" ] && server=${new_root_path%%:*}
 }
 
 # Look through $options, fix "rw"/"ro", move "lock"/"nolock" to $nfslock
